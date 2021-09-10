1d2f7d6d30de3a8f4843ef7ec0dedde79c17d375
stef5924 <stef5924@ZBC-S-STEF5924>
2021-09-10 10:45:17 +0200

    Add project files.

---
+++ Planeterne/Planet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Planeterne
+{
+   public class Planet
+    {
+        static void second(string[] args)
+        {
+         private List<string> mplanets = new List<string>();
+        public List<string> Plantes();
+            //here i've added the planets Merkur, Jorden, Mars, Jupiter, Saturn, Uranus, Neptun, Pluto' in that order.
+            Planets.Add("Merkur");
+            Planets.Add("Jorden");
+            Planets.Add("Mars");
+            Planets.Add("Jupiter");
+            Planets.Add("Saturn");
+            Planets.Add("Uranus");
+            Planets.Add("Neptun");
+            Planets.Add("Pluto");
+            Console.WriteLine();
+            //here i made print out the list
+            foreach (string Planet in Planets)
+            {
+                Console.WriteLine(Planet);
+            }
+        }
+}
+}
---
+++ Planeterne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace Planeterne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Planet);
+        }
+    }
+}
